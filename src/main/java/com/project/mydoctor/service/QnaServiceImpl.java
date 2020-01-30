18e4e8c687b0a2f38d27dadbba91a4de71b665d6
qodbf <qodbf@LAPTOP-BSFAPC77>
2020-01-30 18:28:26 +0900

    Commit[yuri] 2020-01-30 병원디테일 - 문의게시판

--- src/main/java/com/project/mydoctor/service/QnaServiceImpl.java
+++ src/main/java/com/project/mydoctor/service/QnaServiceImpl.java
@@ -10,32 +10,57 @@ import com.project.mydoctor.dao.QnaDAO;
 import com.project.mydoctor.model.Qna;
 import com.project.mydoctor.model.Review;
 
-
 @Service
-public class QnaServiceImpl implements QnaService{
+public class QnaServiceImpl implements QnaService {
 	@Autowired
 	private QnaDAO Qdao;
+
 	@Override
 	public void insertQna(Qna qna) {
 		// TODO Auto-generated method stub
 		Qdao.insertQna(qna);
 	}
+
 	@Override
 	public int getqnaListCount(String id) {
 		// TODO Auto-generated method stub
-		int listcount=Qdao.getListCount(id);
+		int listcount = Qdao.getListCount(id);
 		return listcount;
 	}
+
 	@Override
 	public List<Qna> getMyQnaList(int page, int limit, String id) {
-		HashMap<String, Object> map=new HashMap<String,Object>();
-		int startrow=(page-1)*limit+1;
-		int endrow=startrow+limit-1;
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		int startrow = (page - 1) * limit + 1;
+		int endrow = startrow + limit - 1;
 		map.put("start", startrow);
 		map.put("end", endrow);
 		map.put("id", id);
 		return Qdao.getMyQnaList(map);
 	}
-	
+
+	@Override
+	public int insert(Qna qna) {
+		return Qdao.insert(qna);
+	}
+
+	@Override
+	public int getCount(String hosId) {
+		return Qdao.getCount(hosId);
+	}
+
+	@Override
+	public List<Qna> getQnaList(String hosId, int page, int limit) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+
+		int startrow = (page - 1) * limit + 1;
+		int endrow = startrow + limit - 1;
+
+		map.put("hosId", hosId);
+		map.put("start", startrow);
+		map.put("end", endrow);
+
+		return Qdao.getQnaList(map);
+	}
 
 }
