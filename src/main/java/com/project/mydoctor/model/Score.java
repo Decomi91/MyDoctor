2ef2d4cb3a236750fab98451a7d58f99589e69c5
JH <JH@DESKTOP-7MIB0B2>
2020-02-01 20:33:13 +0900

    Commit [Jiho] : 회원가입 줄맞춤, 병원상세 후기/문의 ajax처리

--- src/main/java/com/project/mydoctor/model/Score.java
+++ src/main/java/com/project/mydoctor/model/Score.java
@@ -8,18 +8,18 @@ public class Score{
 		return kindness;
 	}
 	public void setKindness(double kindness) {
-		this.kindness = kindness;
+		this.kindness = Math.round(kindness*10)/10;
 	}
 	public double getAbility() {
 		return ability;
 	}
 	public void setAbility(double ability) {
-		this.ability = ability;
+		this.ability = Math.round(ability*10)/10;
 	}
 	public double getPrice() {
 		return price;
 	}
 	public void setPrice(double price) {
-		this.price = price;
+		this.price = Math.round(price*10)/10;
 	}
 }
